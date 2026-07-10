5011af24083cf5467ddec251ef1bdc3a65e17910
Jacob <erfnewt@gmail.com>
2026-07-10 15:21:51 -0400

    added services info

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import {Routes, Route} from 'react-router-dom'
+import Layout from './Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+
 
 function App() {
 
   return (
-    <>
-      <h1 className="font-display text-accent text-4xl">Test Heading</h1>
-      <p className="font-body text-muted">Test body text</p>
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='services' element={<Services />} />
+        <Route path='contact' element={<Contact />} />
+      </Route>
+    </Routes>
   )
 }
 
---
+++ src/data/Services.js
@@ -0,0 +1,57 @@
+export const services = [
+    {
+        id: 1,
+        title: 'Weekly Lawn Mow',
+        description: '',
+        price: 70,
+        duration: '30-45 mins',
+        category: ['lawn-care'],
+        season: ['spring', 'summer', 'fall'],
+    },
+    {
+        id: 2,
+        title: 'Spring Cleanup',
+        description: '',
+        price: 225,
+        duration: '1-2 hours',
+        category: ['cleanup'],
+        season: ['spring'],
+    },
+    {
+        id: 3,
+        title: 'Fall Leaf Removal',
+        description: '',
+        price: 150,
+        duration: '1 hour',
+        category: ['cleanup'],
+        season: ['fall'],
+    },
+    {
+        id: 4,
+        title: 'Mulch Installation',
+        description: '',
+        price: '450',
+        duration: '2-3 hours',
+        category: ['landscapng'],
+        season: ['spring', 'fall']
+    },
+    {
+        id: 5,
+        title: 'Gutter Cleaning',
+        description: '',
+        price: 125,
+        duration: '1-2 hours',
+        category: ['cleanup'],
+        season: ['winter', 'summer', 'spring'],
+
+    },
+    {
+        id: 6,
+        title: 'Driveway & Walkway Pressure Washing',
+        description: '',
+        price: 400,
+        duration: '2-3 hours',
+        category: ['cleanup'],
+        season: ['summer','spring','fall'],
+    }
+]
