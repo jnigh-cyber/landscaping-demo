51dbecaeeb652ad2aa5a4c2bd836dabd07a13e9b
Jacob <erfnewt@gmail.com>
2026-07-10 16:50:52 -0400

    Set up the testimonial carousel and linked componet to home page.

--- src/components/TestimonialCarousel.jsx
+++ src/components/TestimonialCarousel.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { useState } from 'react'
+import { testimonials } from '../data/testimonials'
 
 function TestimonialCarousel() {
+
+  const [index, setIndex] = useState(0)
+
+  function handlePrev() {
+    setIndex((prev) => (prev === 0 ? testimonials.length - 1 : prev - 1))
+  }
+
+  function handleNext() {
+    setIndex((prev) => (prev === testimonials.length - 1 ? 0 : prev + 1))
+  }
+
+  const current = testimonials[index]
+
   return (
-    <div>TestimonialCarousel</div>
+    <>
+      <section>
+        <div>
+          <div>
+            {'★'.repeat(current.rating)}
+          </div>
+          <p>{current.name}</p>
+          <p>{current.service}</p>
+          <p>"{current.quote}"</p>
+          
+          <div>
+            <button
+              onClick={handlePrev}
+            >
+              ◄
+            </button>
+            <div>
+              {testimonials.map((t, i) => (
+                <span
+                  key={t.id}>
+
+                </span>
+              ))}
+            </div>
+            <button
+              onClick={handleNext}
+            >
+              ►
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
