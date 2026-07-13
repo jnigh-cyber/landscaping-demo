415dfa8b1117dd72956e61994c9c92fd3fc9f812
Jacob <erfnewt@gmail.com>
2026-07-13 10:51:48 -0400

    Navbar built out not styled.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <div>
+        <Link to='/'>
+          Grass & More Co.
+        </Link>
+        {/* Desktop Links */}
+        <div className='hidden md:flex'>
+          <Link to='/' onClick={() => setIsOpen(false)}>Home</Link>
+          <Link to='/services' onClick={() => setIsOpen(false)}>Services</Link>
+          <Link to='/about' onClick={() => setIsOpen(false)}>About</Link>
+          <Link to='/contact' onClick={() => setIsOpen(false)}>Contact</Link>
+        </div>
+        {/* Mobile Ham button */}
+        <button
+        
+          className='md:hidden'
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label='Toggle Menu'
+        >
+          {isOpen ? '✕' : '☰'}
+        </button>
+      </div>
+
+      {/* Mobile dropdown */}
+      {isOpen && (
+        <div className='md:hidden flex flex-col'>
+          <Link to='/' onClick={() => setIsOpen(false)}>Home</Link>
+          <Link to='/services' onClick={() => setIsOpen(false)}>Services</Link>
+          <Link to='/about' onClick={() => setIsOpen(false)}>About</Link>
+          <Link to='/contact' onClick={() => setIsOpen(false)}>Contact</Link>
+        </div>
+      )}
+
+    </nav>
   )
 }
 
