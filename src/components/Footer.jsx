3f50f5a7331428f1b09d08f07c3423f01ba61c90
Jacob <erfnewt@gmail.com>
2026-07-13 11:06:08 -0400

    Footer built

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Footer() {
+  //Grabing the year.
+  const year = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <footer>
+      <div>
+        <div>
+          <p>Grass & More Co.</p>
+          <p>Lawn care and Landscaping, serving the Triad.</p>
+        </div>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/services'>Services</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+        </nav>
+        <div>
+          <p>[owner@grassandmoreco.com]</p>
+          <p>(336) 123-4567</p>
+        </div>
+      </div>
+      <div>
+        <p>© {year} Grass & More Co. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
